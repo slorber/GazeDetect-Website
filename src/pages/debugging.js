7ee0d0c2a21ef40ca07b286c89e11d8b33bd92f9
Aditya Mangalampalli <aditya.mangalampalli@gmail.com>
2021-02-25 12:06:18 -0800

    Finished basic testing, need to tune model

--- src/pages/debugging.js
+++ src/pages/debugging.js
@@ -1,42 +1,52 @@
 import Layout from "@theme/Layout";
-import React, { useRef } from "react";
+import React, { useRef, useEffect } from "react";
 import * as tf from "@tensorflow/tfjs";
 import * as facemesh from "@tensorflow-models/face-landmarks-detection";
 import Webcam from "react-webcam";
-import {drawMesh} from "./debugging/utilities"
+import { drawMesh } from "../../debugging/utilities";
 
-function EyeTesting() {
-  const faceLandmarksDetection = require("@tensorflow-models/face-landmarks-detection");
+function debugging() {
   const webcamRef = useRef(null);
   const canvasRef = useRef(null);
-  
-  const runModel = async () => {
-    const net = await faceLandmarksDetection.load(faceLandmarksDetection.SupportedPackages.mediapipeFacemesh);
+  const runFacemesh = async () => {
+    const net = await facemesh.load(
+      facemesh.SupportedPackages.mediapipeFacemesh
+    );
     setInterval(() => {
-        detect(net);
-      }, 0);
+      detect(net);
+    }, 0);
   };
-  
+
   const detect = async (net) => {
     if (
-        typeof webcamRef.current !== "undefined" &&
-        webcamRef.current !== null &&
-        webcamRef.current.video.readyState === 4
-      ) {
-        const video = webcamRef.current.video;
-        const videoWidth = webcamRef.current.video.videoWidth;
-        const videoHeight = webcamRef.current.video.videoHeight;
-        webcamRef.current.video.width = videoWidth;
-        webcamRef.current.video.height = videoHeight;
-        canvasRef.current.width = videoWidth;
-        canvasRef.current.height = videoHeight;
-        const face = await net.estimateFaces({input:video});
-        console.log(face);
-        const ctx = canvasRef.current.getContext("2d");
-        requestAnimationFrame(()=>{drawMesh(face, ctx)});
+      typeof webcamRef.current !== "undefined" &&
+      webcamRef.current !== null &&
+      webcamRef.current.video.readyState === 4
+    ) {
+      const video = webcamRef.current.video;
+      const videoWidth = webcamRef.current.video.videoWidth;
+      const videoHeight = webcamRef.current.video.videoHeight;
+
+      webcamRef.current.video.width = videoWidth;
+      webcamRef.current.video.height = videoHeight;
+
+      canvasRef.current.width = videoWidth;
+      canvasRef.current.height = videoHeight;
+
+      const face = await net.estimateFaces({ input: video });
+      console.log(face);
+
+      const ctx = canvasRef.current.getContext("2d");
+      requestAnimationFrame(() => {
+        drawMesh(face, ctx);
+      });
+    }
   };
-}
-  runModel();
+
+  useEffect(() => {
+    runFacemesh();
+  }, []);
+
   return (
     <Layout>
       <Webcam
@@ -45,30 +55,31 @@ function EyeTesting() {
           position: "absolute",
           marginLeft: "auto",
           marginRight: "auto",
-          left: "0",
-          right: "0",
+          left: 0,
+          right: 0,
           textAlign: "center",
-          zIndex: "9",
-          width: "640",
-          height: "480",
+          zindex: 9,
+          width: 640,
+          height: 480,
         }}
       />
+
       <canvas
         ref={canvasRef}
         style={{
           position: "absolute",
           marginLeft: "auto",
           marginRight: "auto",
-          left: "0",
-          right: "0",
+          left: 0,
+          right: 0,
           textAlign: "center",
-          zIndex: "9",
-          width: "640",
-          height: "480",
+          zindex: 9,
+          width: 640,
+          height: 480,
         }}
       />
     </Layout>
   );
 }
 
-export default EyeTesting;
+export default debugging;
