537c77327d208c51a5b558720235179dded6ee3b
Aditya Mangalampalli <aditya.mangalampalli@gmail.com>
2021-02-28 18:46:06 -0800

    Added more diseases

--- sidebars.js
+++ sidebars.js
@@ -1,7 +1,15 @@
 module.exports = {
   mainSidebar: {
-    Calibration: ['calibration/introduction', 'calibration/calibrationModule', 'calibration/webCamTest'],
-    Features: ['mdx'],
-    Tracking: ['tracking/explanation', 'tracking/debugging']
+    "Calibration": ['calibration/introduction', 'calibration/calibrationModule', 'calibration/webCamTest'],
+    "Autism": ['autism/introduction', 'autism/excercise', 'autism/analysis'],
+    "Alzheimer\'s": ['alzheimers/introduction', 'alzheimers/excercise', 'alzheimers/analysis'],
+    "Attention Deficit Hyperactivity Disorder": ['adhd/introduction', 'adhd/excercise', 'adhd/analysis'],
+    "Bell\'s Palsy": ['bellPalsy/introduction', 'bellPalsy/excercise', 'bellPalsy/analysis'],
+    "Huntington\'s Disease": ['huntington/introduction', 'huntington/excercise', 'huntington/analysis'],
+    "Multiple sclerosis": ['multipleSclerosis/introduction', 'multipleSclerosis/excercise', 'multipleSclerosis/analysis'],
+    "Parkinson\'s": ['parkinson/introduction', 'parkinson/excercise', 'parkinson/analysis'],
+    "Schizophrenia": ['schizophrenia/introduction', 'schizophrenia/excercise', 'schizophrenia/analysis'],
+    "Spina Bifida": ['spinaBifida/introduction', 'spinaBifida/excercise', 'spinaBifida/analysis'],
+    "Tracking": ['tracking/explanation', 'tracking/debugging']
   },
 };
